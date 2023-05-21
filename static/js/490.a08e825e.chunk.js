0502a4fd6c33b1c6fb37f8cfe099003d534e728a
Shlov <107765337+Shlov@users.noreply.github.com>
2023-05-21 16:28:30 +0000

    Deploying to gh-pages from @ Shlov/project-42-front@369e04ba351828a4bbbf2a9d4e8099439d079708 🚀

--- static/js/490.a08e825e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_42_front=self.webpackChunkproject_42_front||[]).push([[490],{3490:function(n,e,r){r.r(e);var t=r(184);e.default=function(){return(0,t.jsx)("h2",{children:"Login"})}}}]);
-//# sourceMappingURL=490.a08e825e.chunk.js.map
--- static/js/884.5bd06d8c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_42_front=self.webpackChunkproject_42_front||[]).push([[884],{3884:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("h2",{children:"AddPet"})}}}]);
-//# sourceMappingURL=884.5bd06d8c.chunk.js.map
