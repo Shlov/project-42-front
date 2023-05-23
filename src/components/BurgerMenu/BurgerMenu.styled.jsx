8c7eb9a253893f3d6f5f979e8cd0045f632adeb7
Nadya Mincheva <7000585m1@gmail.com>
2023-05-23 18:06:24 +0300

    burger

--- src/components/BurgerMenu/BurgerMenu.styled.jsx
+++ src/components/BurgerMenu/BurgerMenu.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const MobileMenuContainer = styled.div`
-  overflow-x: hidden;
+overflow: hidden;
   background-color: var(--cl-background);
   height: 100vh;
   width: 100%;
