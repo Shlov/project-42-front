07c46a6206ae16482b2675b2e6001ba34a1b5ecc
Nadya Mincheva <7000585m1@gmail.com>
2023-05-23 15:56:18 +0300

    header

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.js
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.js
@@ -1,3 +1,5 @@
+import {Button} from './NoticesCategoriesNav.styled'
+
 export const NoticesCategoriesNav = ({ setCategoriesArr, categoriesArr, categories, category, setCategory }) => {
   const handleCategory = (categoryItem) => {
     if (categoriesArr.includes(categoryItem.text)) {
@@ -10,10 +12,10 @@ export const NoticesCategoriesNav = ({ setCategoriesArr, categoriesArr, categori
   return (
     <>
       {categories.map((cat, i) =>
-        <label key={i} htmlFor={cat.id}>
+        <Button key={i} htmlFor={cat.id}>
           {cat.text}
           <input type="checkbox" name={cat.name} id={cat.id} value={cat.text} onChange={() => handleCategory(cat)} />
-        </label>
+        </Button>
       )}
     </>
   )
