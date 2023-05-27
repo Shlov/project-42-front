c7e5181e32c19d11c4a3512184d9944d37a046a5
Nadya Mincheva <7000585m1@gmail.com>
2023-05-27 11:49:50 +0300

    fixed the loader

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import { Toaster } from 'react-hot-toast';
 import { Suspense } from "react";
-import Loader from '../Loader/Loader';
+import { Loader } from '../Loader/Loader';
 
 export const SharedLayout = () => {
 
