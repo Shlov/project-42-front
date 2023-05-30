b84554a8eb57d9def2b526508143c3ac2804c6e3
ViraMesha <wira.mescha@gmail.com>
2023-05-30 15:01:31 +0300

    update AddPetButton

--- src/components/AddPetButton/AddPetButton.js
+++ src/components/AddPetButton/AddPetButton.js
@@ -1,21 +1,84 @@
-// Компонент рендерить кнопку-посилання.
-// Для АВТОРИЗОВАНОГО користувача клік по кнопці переадресовує його на сторінку AddPetPage за приватним маршрутом /add-pet
-// Для НЕАВТОРИЗОВАНОГО користувача клік по кнопці виводить повідомлення типу нотіфікація з інформацією про те, що потрібно авторизуватися для використання даного функціоналу
-import { Button, AddIcon } from './AddPetButton.styled.jsx';
+import {
+  Link,
+  AddButton,
+  AddIcon,
+  NavLinkWrapper,
+} from './AddPetButton.styled.jsx';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import icons from 'images/icons.svg';
+import { getConnect } from 'Redux/auth/selector.js';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { ModalApproveAction } from 'components/ModalApproveAction/ModalApproveAction';
+
+import {
+  DescrModal,
+  ModalContent,
+} from './../ModalCongrats/ModalCongrats.styled';
+
+import {
+  ButtonLogin,
+  ButtonRegister,
+  PawCss,
+} from 'components/AuthNav/AuthNav.styled.jsx';
 
 const AddPetButton = () => {
   const location = useLocation();
+  const isLoggedIn = useSelector(getConnect);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpenModal(isOpen => !isOpen);
+  };
+
+  const handleUnauthorizedClick = () => {
+    setIsOpenModal(true);
+  };
 
   return (
     <>
-      <Button to={`/add-pet`} state={{ from: location }}>
-        Add Pet
-        <AddIcon>
-          <use href={icons + '#plus-small'} />
-        </AddIcon>
-      </Button>
+      {isLoggedIn ? (
+        <Link to={`/add-pet`} state={{ from: location }}>
+          Add Pet
+          <AddIcon>
+            <use href={icons + '#plus-small'} />
+          </AddIcon>
+        </Link>
+      ) : (
+        <>
+          <AddButton type="button" onClick={handleUnauthorizedClick}>
+            Add Pet
+            <AddIcon>
+              <use href={icons + '#plus-small'} />
+            </AddIcon>
+          </AddButton>
+          {isOpenModal && (
+            <ModalApproveAction onClose={toggleModal}>
+              <ModalContent>
+                <DescrModal>
+                  Please log in or register to use this functionality.
+                </DescrModal>
+
+                <NavLinkWrapper>
+                  <NavLink to="/login">
+                    <ButtonLogin>
+                      Log IN
+                      <PawCss width={21} height={24}>
+                        <use href={icons + '#pawprint'} />
+                      </PawCss>
+                    </ButtonLogin>
+                  </NavLink>
+                  <NavLink to="/register">
+                    <ButtonRegister>Registration</ButtonRegister>
+                  </NavLink>
+                </NavLinkWrapper>
+              </ModalContent>
+            </ModalApproveAction>
+          )}
+        </>
+      )}
     </>
   );
 };
--- src/components/AddPetButton/AddPetButton.styled.jsx
+++ src/components/AddPetButton/AddPetButton.styled.jsx
@@ -1,7 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-export const Button = styled(NavLink)`
+export const AddIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+  stroke: var(--cl-background);
+  fill: transparent;
+`;
+
+export const Button = css`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -19,18 +26,28 @@ export const Button = styled(NavLink)`
   color: var(--cl-background);
   background-color: var(--cl-blue-link);
 
-  transition: background-color var(--animat), color var(--animat),
-    border-color var(--animat);
+  transition: background-color var(--animat), border-color var(--animat);
 
-  &:hover {
-    background-color: var(--cl-blue-link);
-    color: var(--cl-background);
+  &:hover,
+  &:focus {
+    color: var(--cl-blue-link);
+    background-color: var(--cl-almost-white);
+
+    svg {
+      stroke: var(--cl-blue-link);
+    }
   }
 `;
 
-export const AddIcon = styled.svg`
-  width: 24px;
-  height: 24px;
-  stroke: var(--cl-background);
-  fill: transparent;
+export const Link = styled(NavLink)`
+  ${Button}
+`;
+
+export const AddButton = styled.button`
+  ${Button}
+`;
+
+export const NavLinkWrapper = styled.div`
+  display: flex;
+  gap: 18px;
 `;
