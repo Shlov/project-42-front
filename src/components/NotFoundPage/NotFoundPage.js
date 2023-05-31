7892869625f02807e066b50830b480315d6e38d6
Kateryna Solodilova <kiivikat@gmail.com>
2023-05-31 16:32:28 +0200

    add webp images

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -18,6 +18,13 @@ import desktopNotFoundImg1x from '../../images/error/error@1x_lg.png';
 import desktopNotFoundImg2x from '../../images/error/error@2x_lg.png';
 import { ReactComponent as Paw } from '../../images/icons/pawprint 1.svg';
 
+import mobileNotFoundImg1xWebp from '../../images/error/error-webp@1x_sm.webp';
+import mobileNotFoundImg2xWebp from '../../images/error/error-webp@2x_sm.webp';
+import tabletNotFoundImg1xWebp from '../../images/error/error-webp@1x_md.webp';
+import tabletNotFoundImg2xWebp from '../../images/error/error-webp@2x_md.webp';
+import desktopNotFoundImg1xWebp from '../../images/error/error-webp@1x_lg.webp';
+import desktopNotFoundImg2xWebp from '../../images/error/error-webp@2x_lg.webp';
+
 export const NotFoundPage = () => {
   return (
     <>
@@ -28,8 +35,18 @@ export const NotFoundPage = () => {
         <NotFoundPicture>
           <source
             media="(min-width: 1280px)"
+            srcSet={`${desktopNotFoundImg1xWebp} 1x, ${desktopNotFoundImg2xWebp} 2x`}
+            type="image/webp"
+          />
+          <source
+            media="(min-width: 1280px)"
             srcSet={`${desktopNotFoundImg1x} 1x, ${desktopNotFoundImg2x} 2x`}
             type="image/png"
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet={`${tabletNotFoundImg1xWebp} 1x, ${tabletNotFoundImg2xWebp} 2x`}
+            type="image/webp"
           />
           <source
             media="(min-width: 768px)"
@@ -38,10 +55,20 @@ export const NotFoundPage = () => {
           />
           <source
             media="(max-width: 767px)"
+            srcSet={`${mobileNotFoundImg1xWebp} 1x, ${mobileNotFoundImg2xWebp} 2x`}
+            type="image/webp"
+          />
+          <source
+            media="(max-width: 767px)"
             srcSet={`${mobileNotFoundImg1x} 1x, ${mobileNotFoundImg2x} 2x`}
             type="image/png"
           />
-          <NotFoundImg src={mobileNotFoundImg1x} alt="Cute cat" />
+          <NotFoundImg
+            src={mobileNotFoundImg1x}
+            alt="Cute cat"
+            width="280"
+            height="120"
+          />
         </NotFoundPicture>
         <NotFoundBtn>
           <NotFoundLink to="./">
