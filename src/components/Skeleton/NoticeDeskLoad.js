374f5428f571739c3ce474d63da329b667ecdf56
Anton Shabalov <shabalovanton@gmail.com>
2023-06-02 00:39:37 +0300

    done NoticeSkeletonDesk

--- src/components/Skeleton/NoticeDeskLoad.js
+++ src/components/Skeleton/NoticeDeskLoad.js
@@ -11,24 +11,25 @@ export const NoticeSkeletonDesk = props => (
   >   
 
     <rect x="32" y="32" rx="2" ry="2" width="262" height="298" /> 
-    <rect x="318" y="32" rx="0" ry="0" width="240" height="32" /> 
-    <rect x="318" y="69" rx="0" ry="0" width="170" height="32" /> 
-    <rect x="318" y="122" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="440" y="122" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="318" y="152" rx="0" ry="0" width="70" height="22" /> 
-    <rect x="440" y="152" rx="0" ry="0" width="70" height="22" /> 
-    <rect x="318" y="182" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="400" y="182" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="318" y="212" rx="0" ry="0" width="70" height="22" /> 
-    <rect x="440" y="212" rx="0" ry="0" width="70" height="22" /> 
-    <rect x="318" y="242" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="440" y="242" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="318" y="272" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="440" y="272" rx="0" ry="0" width="50" height="22" /> 
-    <rect x="32" y="360" rx="0" ry="0" width="549" height="17" /> 
-    <rect x="32" y="386" rx="0" ry="0" width="549" height="17" />
-    <rect x="375" y="468" rx="20" ry="20" width="129" height="40" /> 
-    <rect x="516" y="468" rx="20" ry="20" width="129" height="40" /> 
+    <rect x="318" y="32" rx="0" ry="0" width="240" height="38" /> 
+    <rect x="318" y="80" rx="0" ry="0" width="170" height="38" /> 
+    <rect x="318" y="158" rx="0" ry="0" width="50" height="22" /> 
+    <rect x="440" y="158" rx="0" ry="0" width="40" height="22" /> 
+    <rect x="318" y="188" rx="0" ry="0" width="70" height="22" /> 
+    <rect x="440" y="188" rx="0" ry="0" width="82" height="22" /> 
+    <rect x="318" y="218" rx="0" ry="0" width="50" height="22" /> 
+    <rect x="440" y="218" rx="0" ry="0" width="82" height="22" /> 
+    <rect x="318" y="248" rx="0" ry="0" width="70" height="22" /> 
+    <rect x="440" y="248" rx="0" ry="0" width="48" height="22" /> 
+    <rect x="318" y="278" rx="0" ry="0" width="50" height="22" /> 
+    <rect x="440" y="278" rx="0" ry="0" width="116" height="22" /> 
+    <rect x="318" y="308" rx="0" ry="0" width="50" height="22" /> 
+    <rect x="440" y="308" rx="0" ry="0" width="116" height="22" /> 
+    <rect x="32" y="360" rx="0" ry="0" width="618" height="24" /> 
+    <rect x="32" y="390" rx="0" ry="0" width="360" height="24" />
+    {/* <rect x="32" y="420" rx="0" ry="0" width="340" height="24" /> */}
+    <rect x="375" y="476" rx="20" ry="20" width="129" height="40" /> 
+    <rect x="516" y="476" rx="20" ry="20" width="129" height="40" /> 
 
   </ContentLoader>
 )
